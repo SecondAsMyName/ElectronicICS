95a457f6d5f2f5ced4d3ef017506d0514f779617
User <User@LENOVO>
2018-04-09 22:08:54 +0800

    Payment Page

--- ElectronicICS/PaymentPage.Designer.vb
+++ ElectronicICS/PaymentPage.Designer.vb
@@ -22,8 +22,16 @@ Partial Class PaymentPage
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.SuspendLayout()
+        '
+        'PaymentPage
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Name = "PaymentPage"
         Me.Text = "PaymentPage"
+        Me.ResumeLayout(False)
+
     End Sub
 End Class
--- ElectronicICS/PaymentPage.vb
+++ ElectronicICS/PaymentPage.vb
@@ -1,3 +1,5 @@
 ﻿Public Class PaymentPage
+    Private Sub PaymentPage_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
 End Class
