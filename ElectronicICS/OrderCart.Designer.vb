2b629a206df271d359fdf7ffcd1f38be2e37a1fb
lim kah him <kevlim2906@gmail.com>
2018-04-13 14:26:54 +0800

    add back database

--- ElectronicICS/OrderCart.Designer.vb
+++ ElectronicICS/OrderCart.Designer.vb
@@ -23,6 +23,9 @@ Partial Class OrderCart
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ListView1 = New System.Windows.Forms.ListView()
         Me.SuspendLayout()
         '
         'Label1
@@ -31,13 +34,40 @@ Partial Class OrderCart
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(100, 23)
         Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Label1"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(15, 48)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(158, 21)
+        Me.ComboBox1.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.Location = New System.Drawing.Point(12, 22)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(100, 23)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Item Name :"
+        '
+        'ListView1
+        '
+        Me.ListView1.Location = New System.Drawing.Point(249, 22)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(228, 465)
+        Me.ListView1.TabIndex = 3
+        Me.ListView1.UseCompatibleStateImageBehavior = False
         '
         'OrderCart
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(706, 519)
+        Me.Controls.Add(Me.ListView1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ComboBox1)
         Me.Controls.Add(Me.Label1)
         Me.Name = "OrderCart"
         Me.Text = "OrderCart"
@@ -46,4 +76,7 @@ Partial Class OrderCart
     End Sub
 
     Friend WithEvents Label1 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ListView1 As ListView
 End Class
