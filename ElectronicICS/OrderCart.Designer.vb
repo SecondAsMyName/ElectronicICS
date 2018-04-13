204adcf4784bf833ab5dab953d78c3c78819d7aa
lim kah him <kevlim2906@gmail.com>
2018-04-13 14:31:59 +0800

    add db

--- ElectronicICS/OrderCart.Designer.vb
+++ ElectronicICS/OrderCart.Designer.vb
@@ -26,6 +26,7 @@ Partial Class OrderCart
         Me.ComboBox1 = New System.Windows.Forms.ComboBox()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.Label3 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Label1
@@ -56,15 +57,25 @@ Partial Class OrderCart
         '
         Me.ListView1.Location = New System.Drawing.Point(249, 22)
         Me.ListView1.Name = "ListView1"
-        Me.ListView1.Size = New System.Drawing.Size(228, 465)
+        Me.ListView1.Size = New System.Drawing.Size(213, 465)
         Me.ListView1.TabIndex = 3
         Me.ListView1.UseCompatibleStateImageBehavior = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 242)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(52, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Quantity :"
         '
         'OrderCart
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(706, 519)
+        Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.ListView1)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.ComboBox1)
@@ -72,6 +83,7 @@ Partial Class OrderCart
         Me.Name = "OrderCart"
         Me.Text = "OrderCart"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
@@ -79,4 +91,5 @@ Partial Class OrderCart
     Friend WithEvents ComboBox1 As ComboBox
     Friend WithEvents Label2 As Label
     Friend WithEvents ListView1 As ListView
+    Friend WithEvents Label3 As Label
 End Class
