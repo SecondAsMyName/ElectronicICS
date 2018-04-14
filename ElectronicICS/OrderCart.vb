00d603fca78e7fabf8f215a5d29301ae000cd1c3
lim kah him <kevlim2906@gmail.com>
2018-04-14 10:30:48 +0800

    add new field status in order

--- ElectronicICS/OrderCart.vb
+++ ElectronicICS/OrderCart.vb
@@ -9,6 +9,13 @@
         cboItem.ValueMember = "ItemID"
     End Sub
 
+    Private Function IsDuplicatedID(id As Integer) As Boolean
+        Dim db As New DBDataContext()
+        Return db.Orders.Any(Function(o) o.OrderId = id)
+        'True records meets the criteria
+        'FALSE records does not meet the criteria
+    End Function
+
     Private Sub OrderCart_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         BindData()
     End Sub
