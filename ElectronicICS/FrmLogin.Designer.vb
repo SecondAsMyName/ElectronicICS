389e15a95a2c132d8bd1de3c3778cbd8d97cc237
Sing Keat Low <lowsk-wa15@student.tarc.edu.my>
2018-04-14 12:52:12 +0800

    not yet

--- ElectronicICS/FrmLogin.Designer.vb
+++ ElectronicICS/FrmLogin.Designer.vb
@@ -29,9 +29,9 @@ Partial Class FrmLogin
         Me.grTitle = New System.Windows.Forms.GroupBox()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.MaskedTextBox1 = New System.Windows.Forms.MaskedTextBox()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.Button3 = New System.Windows.Forms.Button()
+        Me.btnSignin = New System.Windows.Forms.Button()
+        Me.btnSignup = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
         Me.Label4 = New System.Windows.Forms.Label()
         Me.grTitle.SuspendLayout()
         Me.SuspendLayout()
@@ -95,51 +95,50 @@ Partial Class FrmLogin
         'MaskedTextBox1
         '
         Me.MaskedTextBox1.Location = New System.Drawing.Point(190, 159)
-        Me.MaskedTextBox1.Mask = "AAAAAAAAAAAAaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
         Me.MaskedTextBox1.Name = "MaskedTextBox1"
         Me.MaskedTextBox1.PromptChar = Global.Microsoft.VisualBasic.ChrW(32)
         Me.MaskedTextBox1.Size = New System.Drawing.Size(155, 22)
         Me.MaskedTextBox1.TabIndex = 1
         Me.MaskedTextBox1.UseSystemPasswordChar = True
         '
-        'Button1
+        'btnSignin
         '
-        Me.Button1.BackColor = System.Drawing.Color.LightCoral
-        Me.Button1.Cursor = System.Windows.Forms.Cursors.Hand
-        Me.Button1.Location = New System.Drawing.Point(190, 187)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(82, 30)
-        Me.Button1.TabIndex = 2
-        Me.Button1.Text = "Sign &In"
-        Me.Button1.UseVisualStyleBackColor = False
+        Me.btnSignin.BackColor = System.Drawing.Color.LightCoral
+        Me.btnSignin.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnSignin.Location = New System.Drawing.Point(220, 187)
+        Me.btnSignin.Name = "btnSignin"
+        Me.btnSignin.Size = New System.Drawing.Size(82, 30)
+        Me.btnSignin.TabIndex = 2
+        Me.btnSignin.Text = "Sign &In"
+        Me.btnSignin.UseVisualStyleBackColor = False
         '
-        'Button2
+        'btnSignup
         '
-        Me.Button2.BackColor = System.Drawing.Color.LightCoral
-        Me.Button2.Cursor = System.Windows.Forms.Cursors.Hand
-        Me.Button2.Location = New System.Drawing.Point(190, 237)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(82, 30)
-        Me.Button2.TabIndex = 3
-        Me.Button2.Text = "Sign &Up"
-        Me.Button2.UseVisualStyleBackColor = False
+        Me.btnSignup.BackColor = System.Drawing.Color.LightCoral
+        Me.btnSignup.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnSignup.Location = New System.Drawing.Point(220, 237)
+        Me.btnSignup.Name = "btnSignup"
+        Me.btnSignup.Size = New System.Drawing.Size(82, 30)
+        Me.btnSignup.TabIndex = 3
+        Me.btnSignup.Text = "Sign &Up"
+        Me.btnSignup.UseVisualStyleBackColor = False
         '
-        'Button3
+        'btnExit
         '
-        Me.Button3.Enabled = False
-        Me.Button3.Location = New System.Drawing.Point(392, 231)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(82, 30)
-        Me.Button3.TabIndex = 4
-        Me.Button3.TabStop = False
-        Me.Button3.Text = "E&xit"
-        Me.Button3.UseVisualStyleBackColor = True
-        Me.Button3.Visible = False
+        Me.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnExit.Location = New System.Drawing.Point(392, 231)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(82, 30)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.TabStop = False
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        Me.btnExit.Visible = False
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(218, 220)
+        Me.Label4.Location = New System.Drawing.Point(248, 220)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(24, 17)
         Me.Label4.TabIndex = 5
@@ -147,19 +146,22 @@ Partial Class FrmLogin
         '
         'FrmLogin
         '
+        Me.AcceptButton = Me.btnSignin
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.PaleGreen
-        Me.ClientSize = New System.Drawing.Size(490, 273)
+        Me.CancelButton = Me.btnExit
+        Me.ClientSize = New System.Drawing.Size(490, 281)
         Me.Controls.Add(Me.Label4)
-        Me.Controls.Add(Me.Button3)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnSignup)
+        Me.Controls.Add(Me.btnSignin)
         Me.Controls.Add(Me.MaskedTextBox1)
         Me.Controls.Add(Me.grTitle)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
+        Me.KeyPreview = True
         Me.Name = "FrmLogin"
         Me.Text = "Sign &Out"
         Me.grTitle.ResumeLayout(False)
@@ -175,8 +177,8 @@ Partial Class FrmLogin
     Friend WithEvents grTitle As GroupBox
     Friend WithEvents Label3 As Label
     Friend WithEvents MaskedTextBox1 As MaskedTextBox
-    Friend WithEvents Button1 As Button
-    Friend WithEvents Button2 As Button
-    Friend WithEvents Button3 As Button
+    Friend WithEvents btnSignin As Button
+    Friend WithEvents btnSignup As Button
+    Friend WithEvents btnExit As Button
     Friend WithEvents Label4 As Label
 End Class
--- ElectronicICS/FrmLogin.vb
+++ ElectronicICS/FrmLogin.vb
@@ -1,3 +1,15 @@
 ﻿Public Class FrmLogin
+    Public userName As String
+    Private Sub btnSignin_Click(sender As Object, e As EventArgs) Handles btnSignin.Click
+        Me.Hide()
+        MainPage.Show()
+    End Sub
 
+    Private Sub btnSignup_Click(sender As Object, e As EventArgs) Handles btnSignup.Click
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
 End Class
