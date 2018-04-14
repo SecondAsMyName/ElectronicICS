38212583dc3b84136f8fede06c84c3f3aef44e88
ngwdAlex <33274814+ngwdAlex@users.noreply.github.com>
2018-04-15 01:00:16 +0800

    added with createdelivery page without link

--- ElectronicICS/My Project/Application.Designer.vb
+++ ElectronicICS/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.ElectronicICS.FrmLogin
+            Me.MainForm = Global.ElectronicICS.FrmCreateDelivery
         End Sub
     End Class
 End Namespace
