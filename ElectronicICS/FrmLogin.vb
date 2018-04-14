e0a466ac67a4a3d3bb94416f0511490ee1fb8886
Sing Keat Low <lowsk-wa15@student.tarc.edu.my>
2018-04-14 17:10:00 +0800

    login done

--- ElectronicICS/FrmLogin.vb
+++ ElectronicICS/FrmLogin.vb
@@ -1,15 +1,72 @@
 ﻿Public Class FrmLogin
-    Public userName As String
-    Private Sub btnSignin_Click(sender As Object, e As EventArgs) Handles btnSignin.Click
-        Me.Hide()
-        MainPage.Show()
+    Public user As Security_Account
+    Private Sub BtnSignin_Click(sender As Object, e As EventArgs) Handles btnSignin.Click
+        Dim username As String = txtUsername.Text
+        Dim password As String = mskPassword.Text
+
+        If Me.ValidateChildren = False Then
+            Return
+        End If
+
+        If AuthenticateUser(username, password) Then
+            MainPage.Show()
+            Me.Close()
+        Else
+            ResetForm()
+        End If
     End Sub
 
-    Private Sub btnSignup_Click(sender As Object, e As EventArgs) Handles btnSignup.Click
+    Private Sub BtnSignup_Click(sender As Object, e As EventArgs) Handles btnSignup.Click
 
     End Sub
 
-    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+    Private Sub BtnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
         Me.Close()
+    End Sub
+
+    Private Function AuthenticateUser(userName As String, password As String) As Boolean
+        Dim db As New DBDataContext
+        Dim sc As Security_Account = db.Security_Accounts.FirstOrDefault(Function(o) o.Username = userName)
+
+        If sc Is Nothing Then
+            MessageBox.Show("User name not found.", "Login Fail", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+            Return False
+        Else
+            If sc.Password <> password Then
+                MessageBox.Show("Incorrect Password.", "Login Fail", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+                Return False
+            End If
+        End If
+
+        user = sc
+        Return True
+    End Function
+
+    Private Sub ResetForm()
+        mskPassword.Text = ""
+        mskPassword.Focus()
+        Err.Clear()
+    End Sub
+
+    Private Sub txtUsername_Validating(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles txtUsername.Validating
+        Dim username As String = txtUsername.Text
+
+        If username = "" Then
+            errLogin.SetError(txtUsername, "Please enter user name.")
+            e.Cancel = True
+        Else
+            errLogin.SetError(txtUsername, Nothing)
+        End If
+    End Sub
+
+    Private Sub mskPassword_Validating(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles mskPassword.Validating
+        Dim password As String = mskPassword.Text
+
+        If password = "" Then
+            errLogin.SetError(mskPassword, "Please enter password.")
+            e.Cancel = True
+        Else
+            errLogin.SetError(mskPassword, Nothing)
+        End If
     End Sub
 End Class
--- ElectronicICS/MainPage.Designer.vb
+++ ElectronicICS/MainPage.Designer.vb
@@ -23,23 +23,58 @@ Partial Class MainPage
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.btnDeliveryMenu = New System.Windows.Forms.Button()
+        Me.btnOrderForm = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'btnDeliveryMenu
         '
-        Me.btnDeliveryMenu.Location = New System.Drawing.Point(35, 123)
+        Me.btnDeliveryMenu.Location = New System.Drawing.Point(209, 147)
+        Me.btnDeliveryMenu.Margin = New System.Windows.Forms.Padding(4)
         Me.btnDeliveryMenu.Name = "btnDeliveryMenu"
-        Me.btnDeliveryMenu.Size = New System.Drawing.Size(75, 23)
+        Me.btnDeliveryMenu.Size = New System.Drawing.Size(100, 28)
         Me.btnDeliveryMenu.TabIndex = 0
         Me.btnDeliveryMenu.Text = "Delivery"
         Me.btnDeliveryMenu.UseVisualStyleBackColor = True
         '
+        'btnOrderForm
+        '
+        Me.btnOrderForm.Location = New System.Drawing.Point(46, 30)
+        Me.btnOrderForm.Name = "btnOrderForm"
+        Me.btnOrderForm.Size = New System.Drawing.Size(75, 23)
+        Me.btnOrderForm.TabIndex = 1
+        Me.btnOrderForm.Text = "Order"
+        Me.btnOrderForm.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(240, 61)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(62, 147)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
         'MainPage
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.ClientSize = New System.Drawing.Size(379, 321)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.btnOrderForm)
         Me.Controls.Add(Me.btnDeliveryMenu)
+        Me.Margin = New System.Windows.Forms.Padding(4)
         Me.Name = "MainPage"
         Me.Text = "ElectronicICS"
         Me.ResumeLayout(False)
@@ -47,4 +82,7 @@ Partial Class MainPage
     End Sub
 
     Friend WithEvents btnDeliveryMenu As Button
+    Friend WithEvents btnOrderForm As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
 End Class
