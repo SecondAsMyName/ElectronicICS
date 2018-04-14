4ad8fe0d128ead2719028b7a9fc1aa8ed88181a5
lim kah him <kevlim2906@gmail.com>
2018-04-14 09:34:47 +0800

    pls delete the log file, it will corrupt database

--- ElectronicICS/OrderCart.vb
+++ ElectronicICS/OrderCart.vb
@@ -3,14 +3,12 @@
         Dim db As New DBDataContext()
 
         Dim rs = From o In db.Items
-
-
-
+        cboItem.Items.Add(rs)
 
     End Sub
 
     Private Sub OrderCart_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
+        BindData()
     End Sub
 
 End Class
