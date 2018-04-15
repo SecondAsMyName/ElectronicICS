ab68ac10e306fd8a1038cc534d376c120412c4ab
ngwdAlex <33274814+ngwdAlex@users.noreply.github.com>
2018-04-15 15:27:34 +0800

    full functionality of CreateDelivery

--- ElectronicICS/FrmCreateDelivery.vb
+++ ElectronicICS/FrmCreateDelivery.vb
@@ -17,13 +17,61 @@
     End Function
 
     Private Sub getOrderID()
+
         Dim db As New DBDataContext()
         Dim getID = From o In db.Orders Where o.OrderStatus = "Complete" Select o.OrderId
-        ddlOrderID.DataSource = getID
+
+        Dim getAssignedID = From a In db.Deliveries Select a.OrderId
+        'arr.Equals(getID.Except(getAssignedID).Union(getAssignedID.Except(getID)))
+        'Dim assignID As Integer = Integer.Parse(getAssignedID.ToString)
+        'If Not getID.Contains(assignID) Then
+        '    arr.Add(getAssignedID.
+        'End If
+        ddlOrderID.DataSource = getID.Except(getAssignedID).Union(getAssignedID.Except(getID))
+        'For Each item As Delivery In db.Deliveries
+        '    If getAssignedID.Equals(getID) Then
+
+
+        '    End If
+        'Next
+
+
+
 
     End Sub
 
     Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
         Me.Hide()
+    End Sub
+
+    Private Sub btnCreate_Click(sender As Object, e As EventArgs) Handles btnCreate.Click
+        Dim DeliveryID As Integer = Integer.Parse(lblDeliveryID.Text)
+        Dim delivery As New Delivery
+
+        With delivery
+            .DeliveryId = DeliveryID
+            .Date = System.DateTime.Now
+            .Status = "Delivering"
+            .OrderId = Integer.Parse(ddlOrderID.SelectedItem.ToString)
+        End With
+        Dim db As New DBDataContext()
+        db.Deliveries.InsertOnSubmit(delivery)
+
+        Try
+            db.SubmitChanges()
+        Catch err As Exception
+            MessageBox.Show("Error. Please Try Again.")
+        End Try
+
+        MessageBox.Show("Delivery has been made", "Delivery Creation Successful.")
+
+        FormReset()
+        getOrderID()
+    End Sub
+
+    Private Sub FormReset()
+        lblDeliveryID.Text = getLastID().ToString
+        ddlOrderID.SelectedIndex = -1
+
     End Sub
 End Class
